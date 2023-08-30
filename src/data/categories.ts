bf47b53261a09c1b182cc272b7d3bee46deb5784
Gustavo Satig <gustavo.satig@gmail.com>
2023-08-30 19:15:34 -0300

    Budget tracker

---
+++ src/data/categories.ts
@@ -0,0 +1,120 @@
+import { Category } from "../types/Category";
+
+export const categories: Category = {
+    food: {
+      title: "Alimentação",
+      color: "blue",
+      expense: true,
+    },
+    rent: {
+      title: "Moradia",
+      color: "brown",
+      expense: true,
+    },
+    transport: {
+      title: "Transporte",
+      color: "purple",
+      expense: true,
+    },
+    health: {
+      title: "Saúde",
+      color: "red",
+      expense: true,
+    },
+    education: {
+      title: "Educação",
+      color: "orange",
+      expense: true,
+    },
+    entertainment: {
+      title: "Entretenimento",
+      color: "pink",
+      expense: true,
+    },
+    utilities: {
+      title: "Utilidades",
+      color: "teal",
+      expense: true,
+    },
+    clothing: {
+      title: "Vestuário",
+      color: "indigo",
+      expense: true,
+    },
+    taxes: {
+      title: "Impostos",
+      color: "cyan",
+      expense: true,
+    },
+    debt: {
+      title: "Dívidas",
+      color: "amber",
+      expense: true,
+    },
+    maintenance: {
+      title: "Manutenção",
+      color: "lime",
+      expense: true,
+    },
+    donations: {
+      title: "Doações e Caridade",
+      color: "red",
+      expense: true,
+    },
+    investments: {
+      title: "Investimentos",
+      color: "green",
+      expense: true,
+    },
+    otherExpenses: {
+      title: "Outras Despesas",
+      color: "grey",
+      expense: true,
+    },
+    salary: {
+      title: "Salário",
+      color: "green",
+      expense: false,
+    },
+    freelance: {
+      title: "Trabalho Autônomo",
+      color: "blue",
+      expense: false,
+    },
+    rentalIncome: {
+      title: "Renda de Aluguel",
+      color: "purple",
+      expense: false,
+    },
+    investmentsIncome: {
+      title: "Renda de Investimentos",
+      color: "green",
+      expense: false,
+    },
+    sales: {
+      title: "Vendas",
+      color: "pink",
+      expense: false,
+    },
+    refunds: {
+      title: "Reembolsos",
+      color: "teal",
+      expense: false,
+    },
+    prizes: {
+      title: "Prêmios",
+      color: "indigo",
+      expense: false,
+    },
+    financialAid: {
+      title: "Ajuda Financeira",
+      color: "amber",
+      expense: false,
+    },
+    otherIncome: {
+      title: "Outras Fontes de Renda",
+      color: "grey",
+      expense: false,
+    },
+  };
+  
---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  // {
+  //   date: new Date(2023, 7, 5),
+  //   category: "food",
+  //   title: "McDonalds",
+  //   value: 32.12,
+  // },
+  // {
+  //   date: new Date(2023, 7, 15),
+  //   category: "food",
+  //   title: "Burger King",
+  //   value: 29.00,
+  // },
+  // {
+  //   date: new Date(2023, 7, 16),
+  //   category: "rent",
+  //   title: "Aluguel Apto",
+  //   value: 800.00,
+  // },
+  // {
+  //   date: new Date(2023, 9, 18),
+  //   category: "salary",
+  //   title: "Salário SETI",
+  //   value: 3200.00,
+  // },
+];
